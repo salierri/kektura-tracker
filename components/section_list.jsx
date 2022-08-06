8f7cb13cec5f80ac12c93fa1353a21206d02a3ad
Gulyás Gergely <g.gergely314@gmail.com>
2022-08-06 16:30:11 +0200

    Sub-section draft

--- components/section_list.jsx
+++ components/section_list.jsx
@@ -20,9 +20,10 @@ export default function SectionList() {
 
   return (
     <table className="table table-striped table-bordered table-hover table-sm">
-      <tbody>
         { sections.map((section, index) => {
           return (
+            <>
+            <tbody>
             <tr key={section.id}>
               <td className="text-center">
                 <h4 className="d-inline"><b>{section.id}</b></h4>
@@ -33,10 +34,34 @@ export default function SectionList() {
               <td className="text-center">
                 <input type="checkbox" className="big-checkbox" checked={completeList[index]} onChange={e => changeCompleted(index, e)} />
               </td>
+              <td>
+              <a role="button" data-toggle="collapse" href={`#coll${section.id}`} aria-controls={`coll${section.id}`} aria-expanded="false">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="black" class="bi bi-caret-down-fill" viewBox="0 0 16 16">
+                  <path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z"/>
+                </svg>
+              </a>
+              </td>
             </tr>
+      </tbody>
+      <tbody id={`coll${section.id}`} className="collapse">
+            { section.subSections.map((subSection, index) => {
+              return (
+                <tr>
+                  <td className="text-center">
+                  </td>
+                  <td className="text-center">
+                    <h7 className="d-inline">{subSection.start} - {subSection.end}</h7>
+                  </td>
+                  <td className="text-center">
+                    <input type="checkbox" className="medium-checkbox" checked={completeList[index]} onChange={e => changeCompleted(index, e)} />
+                  </td>
+                </tr>
+              );
+            }) }
+            </tbody>
+            </>
           )
         })}
-      </tbody>
     </table>
   )
 }
